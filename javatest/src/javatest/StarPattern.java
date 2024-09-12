cced81664357a48bf155af2d52a78b65371a90a0
Vignesh26301 <gv3359890@gmail.com>
2024-09-12 12:50:45 +0530

    commit

--- javatest/src/javatest/StarPattern.java
+++ javatest/src/javatest/StarPattern.java
@@ -2,27 +2,42 @@ package javatest;
 
 import java.util.Scanner;
 
-public class StarPattern {
+public class StarPattern 
 
-	public static void main(String[] args) {
+{
+
+	public static void main(String[] args) 
+		{
+	    int i,j,k,n;
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter the number of rows:");
-		int n =scanner.nextInt();
-		scanner.close();
+		 n =scanner.nextInt();
+		for (i=0;i<=n-1;i ++)
+		{
 		
-		for(int i=n;i>0;i--) {
-			
-			for (int j=0;j<i;j++) {
-				System.out.println("*");
-			}
-			System.out.println();
-			}
-		
-		for (int i=1;i<=n;i++) {
-			System.out.print("*");
+		for(j=0;j<i;j++)
+		{
+			System.out.print(" ");
 		}
-		System.out.println();
-
-	}
-
-}
+		for(k=i;k<=n-1;k++)
+		{
+			System.out.print("*" + " " );
+		}
+		System.out.println("");
+		}
+		for (i=n-1;i>= 0;i--)
+		{
+			for(j=0;j<i;j++)
+			{
+				System.out.print(" ");
+			}
+			for (k=i;k<=n-1; k++)
+			{System.out.print("*" + " ");
+			}
+			System.out.println("");
+		}
+			scanner.close();
+		}
+		
+		}
+	
